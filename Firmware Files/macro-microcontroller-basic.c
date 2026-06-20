7947ecf0a24365411c3ba68e367ade5af48a4658
Nicholas Lim <nicholas_lim@bbsshack.club>
2026-06-20 12:15:54 +0800

    Upload files

---
+++ Firmware Files/macro-microcontroller-basic.c
@@ -0,0 +1,215 @@
+// SPDX-License-Identifier: MIT
+/*
+Macro Microcontroller BASIC, Version 0.0.1.
+PCB files for Macro Microcontroller 2 are available at
+https://github.com/Nicholas1023/macro-microcontroller-2.
+
+MIT License
+
+Copyright (C) 2025-2026 Nicholas Lim.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "pico/sync.h"
+
+void interpretermain();
+void gpiocontrol();
+
+const int SW1 = 0;
+const int SW2 = 1;
+const int SW3 = 2;
+
+const int IO1 = 3;
+const int IO2 = 4;
+const int IO3 = 5;
+
+const int LED1 = 6;
+const int LED2 = 7;
+const int LED3 = 8;
+
+const int BELL = 9;
+const int MOTOR = 10;
+
+extern char end;
+char *variable[] = {"", ""};
+char input[51];
+char userinput[51];
+char command[51];
+int line_count = 0;
+
+void main() {
+    stdio_init_all();
+    sleep_ms(1500);
+    gpio_init(SW1);
+    gpio_init(SW2);
+    gpio_init(SW3);
+    gpio_init(IO1);
+    gpio_init(IO2);
+    gpio_init(IO3);
+    gpio_init(LED1);
+    gpio_init(LED2);
+    gpio_init(LED3);
+    gpio_init(BELL);
+    gpio_init(MOTOR);
+    gpio_set_dir(SW1, GPIO_IN);
+    gpio_set_dir(SW2, GPIO_IN);
+    gpio_set_dir(SW3, GPIO_IN);
+    gpio_set_dir(IO1, GPIO_OUT);
+    gpio_set_dir(IO2, GPIO_OUT);
+    gpio_set_dir(IO3, GPIO_OUT);
+    gpio_set_dir(LED1, GPIO_OUT);
+    gpio_set_dir(LED2, GPIO_OUT);
+    gpio_set_dir(LED3, GPIO_OUT);
+    gpio_set_dir(BELL, GPIO_OUT);
+    gpio_set_dir(MOTOR, GPIO_OUT);
+    const int testVariable = 1;
+    if (testVariable == 1) {
+        gpio_put(LED1, 1);
+        gpio_put(LED2, 1);
+        gpio_put(LED3, 1);
+        gpio_put(BELL, 1);
+        sleep_ms(100);
+        gpio_put(LED1, 0);
+        gpio_put(LED2, 0);
+        gpio_put(LED3, 0);
+        gpio_put(BELL, 0);
+        printf("Macro Microcontroller BASIC Version 0.0.1.\nCopyright (C) 2025-2026 Nicholas Lim.\n");
+        volatile uint32_t stack_var;
+        printf("%d bytes of RAM free.\n", (uint32_t)&stack_var - (uint32_t)&end);
+        printf("Ready.\n");
+        interpretermain();
+    } else {
+        printf("Error detected in RAM. Retrying in 5 seconds...\n");
+        gpio_put(LED1, 1);
+        gpio_put(LED2, 1);
+        gpio_put(LED3, 1);
+        gpio_put(BELL, 1);
+        sleep_ms(100);
+        gpio_put(LED1, 0);
+        gpio_put(LED2, 0);
+        gpio_put(LED3, 0);
+        gpio_put(BELL, 0);
+        sleep_ms(100);
+        gpio_put(LED1, 1);
+        gpio_put(LED2, 1);
+        gpio_put(LED3, 1);
+        gpio_put(BELL, 1);
+        sleep_ms(100);
+        gpio_put(LED1, 0);
+        gpio_put(LED2, 0);
+        gpio_put(LED3, 0);
+        gpio_put(BELL, 0);
+        sleep_ms(5000);
+        main();
+    }
+}
+
+void interpretermain() {
+    while (true) {
+        line_count++;
+        printf("%d0 ", line_count);
+        memset(input, 0, 51);
+        fgets(input, 51, stdin);
+        printf("%s", input);
+        char *statement = strtok(input, " ");
+        if (statement == NULL) continue;
+        if (strcasecmp(statement, "PRINT") == 0) {
+            statement = strtok(NULL, "\n");
+            if (statement == NULL) continue;
+            if (strcasecmp(statement, variable[0]) == 0) {
+                statement = variable[1];
+                if (*statement == '"') {
+                    statement++;
+                    char *close = strrchr(statement, '"');
+                    *close = '\0';
+                }
+                printf("%s\n", statement);
+            } else if (*statement == '"') {
+                statement++;
+                char *close = strrchr(statement, '"');
+                *close = '\0';
+                printf("%s\n", statement);
+            } else if (strcasecmp(statement, "BELL") == 0) {
+                gpio_put(BELL, 1);
+                sleep_ms(100);
+                gpio_put(BELL, 0);
+                printf("BELL\n");
+            }
+
+        } else if (strcasecmp(statement, "GPIO") == 0) {
+            statement = strtok(NULL, "\n");
+            if (statement == NULL) continue;
+            if (strcasecmp(statement, "IO1") == 0) gpiocontrol(IO1);
+            else if (strcasecmp(statement, "IO2") == 0) gpiocontrol(IO2);
+            else if (strcasecmp(statement, "IO3") == 0) gpiocontrol(IO3);
+            else if (strcasecmp(statement, "LED1") == 0) gpiocontrol(LED1);
+            else if (strcasecmp(statement, "LED2") == 0) gpiocontrol(LED2);
+            else if (strcasecmp(statement, "LED3") == 0) gpiocontrol(LED3);
+            else if (strcasecmp(statement, "BELL") == 0) gpiocontrol(BELL);
+            else if (strcasecmp(statement, "MOTOR") == 0) gpiocontrol(MOTOR);
+            else printf("Error: Invalid GPIO pin number. Valid pins: IO1, IO2, IO3, LED1, LED2, LED3, BELL, MOTOR\n");
+
+        } else if (strcasecmp(statement, "INPUT") == 0) {
+            statement = strtok(NULL, "\n");
+            if (statement == NULL) continue;
+            printf("%s? ", statement);
+            fgets(userinput, 51, stdin);
+            printf("%s", userinput);
+            char *value = strtok(userinput, "\n");
+            variable[0] = strdup(statement);
+            variable[1] = value;
+
+        } else if (strcasecmp(statement, "LET") == 0) {
+            statement = strtok(NULL, " ");
+            if (statement == NULL) continue;
+            variable[0] = strdup(statement);
+            statement = strtok(NULL, " ");
+            statement = strtok(NULL, "\n");
+            variable[1] = strdup(statement);
+
+        } else if (strcasecmp(statement, "END\n") == 0) {
+            variable[0] = "";
+            variable[1] = "";
+            line_count = 0;
+            printf("Ready.\n");
+        } else if (strcasecmp(statement, "EXIT\n") == 0) {
+            variable[0] = "";
+            variable[1] = "";
+            line_count = 0;
+            while (1) __wfi();
+        
+        } else if (strcasecmp(statement, "LSVAR\n") == 0) {
+            if (variable[0] != "") {
+                printf("Variable %s ", variable[0]);
+                printf("with value %s\n", variable[1]);
+            }
+            
+        } else { printf("Error: Invalid statement.\n"); }
+    }
+}
+
+void gpiocontrol(int pin) {
+    gpio_put(pin, 1);
+    sleep_ms(1000);
+    gpio_put(pin, 0);
+}
---
+++ README.md
@@ -0,0 +1,72 @@
+# Macro Microcontroller 2
+A macropad and RP2040 microcontroller development board that allows you to control GPIOs with keyboard keys combined on a single PCB, with a custom BASIC interpreter.
+
+<img src="Assets/macro-microcontroller-2.png">
+
+## What's New?
+1. Microcontroller unit directly soldered on board!
+2. More output devices with speed and volume adjustments (Buzzer and motor).
+3. More ways to power the board (Barrel jack, USB, GPIO pins).
+
+## What Does it Do?
+Macro Microcontroller 2 allows you to control GPIO pins using macropad-like keys and from the serial terminal using the built-in <a href="Firmware Files/macro-microcontroller-basic.c">Macro Microcontroller BASIC Interpreter</a>. It offers more ways to control GPIOs by having builtin keys that can be programmed to your own needs.
+
+## Enclosure
+<img src="Assets/enclosure-image.png">
+
+## Macro Microcontroller BASIC Interpreter (Default Firmware)
+A BASIC interpreter written in C for Macro Microcontroller, providing a simple coding environment via a serial terminal. Currently available statements:
+1. `PRINT`: Outputs text to the serial terminal.
+2. `LET`: Assigns a value (integer, float or string) to a variable (Only one variable is allowed for now).
+3. `INPUT`: Assigns a value (integer, float or string) to a variable via user input.
+4. `GPIO`: Turns on a specified GPIO pin for 1 second.
+5. `LSVAR`: Outputs variable name and their value to the serial terminal.
+6. `END`: Clears variable and reset line numbers.
+7. `EXIT`: Puts the device into sleep/power off mode.
+
+## Schematics
+<img src="Assets/schematics.svg">
+
+View PCB file on https://kicanvas.org/?repo=https%3A%2F%2Fgithub.com%2FNicholas1023%2Fmacro-microcontroller-2%2Fblob%2Fmain%2FPCB%20Files%2Fmacro-microcontroller-2.kicad_pcb!
+
+## Repository Structure
+```
+main
+ ├── /Assets ← Images and videos are here
+ ├── /PCB Files ← .kicad_ files are here
+ ├── /Production Files ← Gerber and BOM files are here
+ ├── /Printing Files ← STLs are here
+ ├── /Firmware Files ← Firmware files are here
+ │     ├── macro-microcontroller-basic.c ← Firmware source file
+ │     └── macro-microcontroller-basic.uf2 ← Compiled firmware
+ │
+ ├── README.md ← This file
+ ├── LICENSE.txt ← License
+ └── schematics.pdf ← Schematics
+ ```
+
+## Bill of Materials (Excluding DNP)
+|Component|Ref Designator|Qty|Footprint|
+|---------|--------------|---|---------|
+|TMB12A24|BZ1|1|Buzzer_12x9.5RM7.6|
+|Capacitor 0.1uF|C1, C2, C3, C4, C5, C6, C7, C10, C11|9|0402|
+|Capacitor 8pF|C8, C9|2|0402|
+|THT LED|D1, D2, D3|3|LED_D5.0mm|
+|SMD LED|D4|1|0603|
+|USB-B Micro Port|J1|1|USB_Micro-B_Molex_47346-0001|
+|Barrel Jack|J3|1|BarrelJack_Horizontal|
+|2.54 1x05 Pin|J4|1|PinHeader_1x05_P2.54mm_Vertical|
+|2.54 1x02 Pin|M1|1|PinHeader_1x02_P2.54mm_Vertical|
+|Resistor 27ohm|R1, R6|2|0402|
+|Resistor 220ohm|R2, R4, R5, R7|4|0402|
+|Resistor 5kohm|R3, R8|2|0402|
+|Potentiometer|RV1, RV2|2|Potentiometer_Vishay_T73YP_Vertical|
+|Cherry MX Keys|SW1, SW2, SW3|3|SW_Cherry_MX_1.00u_PCB|
+|Reset & Boot Button|SW4, SW6|2|SW_Push_SPST_NO_Alps_SKRK|
+|DIP Switch|SW5|1|SW_DIP_SPSTx02_Slide_9.78x7.26mm_W7.62mm_P2.54mm|
+|RP2040|U1|1|QFN-56-1EP_7x7mm_P0.4mm_EP3.2x3.2mm|
+|TLV71311PDBVR|U2|1|SOT-23-5|
+|XC6206P332MR|U3|1|SOT-23-3|
+|W25Q128JVP|U4|1|WSON-8-1EP_6x5mm_P1.27mm_EP3.4x4.3mm|
+|TPS76350|U6|1|SOT-23-5|
+|NX3225SA-12MHz|Y1|1|Crystal_SMD_3225-4Pin_3.2x2.5mm|
